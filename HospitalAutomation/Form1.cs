41a7b5050277125d5d9cdbc323caf7918da9a5be
gamzetu <gamzetuna08@gmail.com>
2022-05-09 01:35:52 +0300

    gamze deneme

--- HospitalAutomation/Form1.cs
+++ HospitalAutomation/Form1.cs
@@ -20,6 +20,7 @@ namespace HospitalAutomation
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("Test 1");
+            MessageBox.Show("gamze");
         }
     }
 }
