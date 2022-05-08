fc494104d9b2f3b93f0cca8c8836ff636abf1724
Ozan Kiraz <ozankiiraz@gmail.com>
2022-05-09 01:29:27 +0300

    OZAN COMMIT

--- HospitalAutomation/Form1.cs
+++ HospitalAutomation/Form1.cs
@@ -20,6 +20,7 @@ namespace HospitalAutomation
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("Test 4");
+            MessageBox.Show("OZAN MBOX");
         }
     }
 }
