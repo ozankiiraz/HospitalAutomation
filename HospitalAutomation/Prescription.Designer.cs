3ce6bc7e9213b8bf186a8bd03efa9a87f30c1584
emreayvaz <eayvaz99@gmail.com>
2022-05-13 01:33:22 +0300

    reçete oluşturma formu eklendi

---
+++ HospitalAutomation/Prescription.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace HospitalAutomation
+{
+    partial class Prescription
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewIlaclar = new System.Windows.Forms.DataGridView();
+            this.dataGridViewRecete = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewIlaclar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRecete)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewIlaclar
+            // 
+            this.dataGridViewIlaclar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewIlaclar.Location = new System.Drawing.Point(12, 152);
+            this.dataGridViewIlaclar.Name = "dataGridViewIlaclar";
+            this.dataGridViewIlaclar.Size = new System.Drawing.Size(380, 417);
+            this.dataGridViewIlaclar.TabIndex = 0;
+            this.dataGridViewIlaclar.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewIlaclar_CellClick);
+            // 
+            // dataGridViewRecete
+            // 
+            this.dataGridViewRecete.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewRecete.Location = new System.Drawing.Point(775, 152);
+            this.dataGridViewRecete.Name = "dataGridViewRecete";
+            this.dataGridViewRecete.Size = new System.Drawing.Size(413, 417);
+            this.dataGridViewRecete.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(508, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = ">>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(508, 365);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "<<";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 592);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(1176, 57);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(13, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "T.C Kimlik No :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(128, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 26);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(128, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 26);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(13, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "İlaç Adı :";
+            // 
+            // Prescription
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 661);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridViewRecete);
+            this.Controls.Add(this.dataGridViewIlaclar);
+            this.Name = "Prescription";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Prescription_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewIlaclar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRecete)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewIlaclar;
+        private System.Windows.Forms.DataGridView dataGridViewRecete;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ HospitalAutomation/Prescription.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HospitalAutomation
+{
+    public partial class Prescription : Form
+    {
+        public Prescription()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Hastane"].ConnectionString);
+        DataSet PrescriptionTablolar = new DataSet();
+        int medicineID;
+
+        private void Prescription_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter ilaclar = new SqlDataAdapter("SELECT MedicineID,MedicineName FROM Medicines where [Status]=1", con);
+            ilaclar.Fill(PrescriptionTablolar);
+            dataGridViewIlaclar.DataSource = PrescriptionTablolar.Tables[0];
+            dataGridViewIlaclar.Columns[0].Visible = false;
+            dataGridViewIlaclar.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void dataGridViewIlaclar_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            medicineID = Convert.ToInt32(dataGridViewIlaclar.CurrentRow.Cells[0].Value);
+            MessageBox.Show(medicineID.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter ilaclar2 = new SqlDataAdapter("SELECT MedicineID,MedicineName FROM Medicines where [Status]=1 and MedicineID=@medicineId", con);
+            DataTable dt = new DataTable();
+            ilaclar2.SelectCommand.Parameters.AddWithValue("@medicineId", medicineID);
+            ilaclar2.Fill(dt);
+            dataGridViewRecete.DataSource = dt;
+            dataGridViewRecete.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+    }
+}
