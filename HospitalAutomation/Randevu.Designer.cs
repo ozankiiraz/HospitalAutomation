dfe7f142a40208d8f1dab940bde304efc057798a
Kübra Yıldırım <96kbrayldrm@gmail.com>
2022-05-25 00:17:53 +0300

    kubra

--- HospitalAutomation/Randevu.Designer.cs
+++ HospitalAutomation/Randevu.Designer.cs
@@ -29,11 +29,13 @@ namespace HospitalAutomation
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button3 = new System.Windows.Forms.Button();
+			this.components = new System.ComponentModel.Container();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.button4 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.button1 = new System.Windows.Forms.Button();
-			this.dataGridView1 = new System.Windows.Forms.DataGridView();
-			this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
 			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
 			this.comboBox2 = new System.Windows.Forms.ComboBox();
 			this.comboBox1 = new System.Windows.Forms.ComboBox();
@@ -43,153 +45,246 @@ namespace HospitalAutomation
 			this.label3 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.label1 = new System.Windows.Forms.Label();
+			this.button3 = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.hOSPITALDataSet = new HospitalAutomation.HOSPITALDataSet();
+			this.patientsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+			this.patientsTableAdapter = new HospitalAutomation.HOSPITALDataSetTableAdapters.PatientsTableAdapter();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.groupBox1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.hOSPITALDataSet)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.patientsBindingSource)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// button3
+			// groupBox1
 			// 
-			this.button3.Location = new System.Drawing.Point(602, 396);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(87, 34);
-			this.button3.TabIndex = 41;
-			this.button3.Text = "Göster";
-			this.button3.UseVisualStyleBackColor = true;
-			this.button3.Click += new System.EventHandler(this.button3_Click);
+			this.groupBox1.BackColor = System.Drawing.Color.SteelBlue;
+			this.groupBox1.Controls.Add(this.textBox3);
+			this.groupBox1.Controls.Add(this.button4);
+			this.groupBox1.Controls.Add(this.button2);
+			this.groupBox1.Controls.Add(this.label6);
+			this.groupBox1.Controls.Add(this.textBox2);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.dateTimePicker1);
+			this.groupBox1.Controls.Add(this.comboBox2);
+			this.groupBox1.Controls.Add(this.comboBox1);
+			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.Controls.Add(this.label5);
+			this.groupBox1.Controls.Add(this.label4);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.groupBox1.Location = new System.Drawing.Point(0, 14);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(229, 297);
+			this.groupBox1.TabIndex = 49;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Yeni Randevu";
+			// 
+			// button4
+			// 
+			this.button4.ImageIndex = 3;
+			this.button4.Location = new System.Drawing.Point(182, 24);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(38, 31);
+			this.button4.TabIndex = 43;
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
 			// 
 			// button2
 			// 
-			this.button2.Location = new System.Drawing.Point(84, 228);
+			this.button2.BackColor = System.Drawing.Color.LightBlue;
+			this.button2.ImageIndex = 1;
+			this.button2.Location = new System.Drawing.Point(170, 219);
 			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(72, 42);
-			this.button2.TabIndex = 40;
-			this.button2.Text = "Güncelle";
-			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Size = new System.Drawing.Size(48, 46);
+			this.button2.TabIndex = 56;
+			this.button2.UseVisualStyleBackColor = false;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(32, 59);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(63, 17);
+			this.label6.TabIndex = 58;
+			this.label6.Text = "Hasta No";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(99, 178);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(121, 20);
+			this.textBox2.TabIndex = 57;
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(12, 228);
+			this.button1.BackColor = System.Drawing.Color.LightBlue;
+			this.button1.ImageIndex = 0;
+			this.button1.Location = new System.Drawing.Point(99, 219);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(66, 42);
-			this.button1.TabIndex = 39;
-			this.button1.Text = "Kaydet";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// dataGridView1
-			// 
-			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Location = new System.Drawing.Point(220, 21);
-			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.Size = new System.Drawing.Size(469, 369);
-			this.dataGridView1.TabIndex = 38;
-			// 
-			// maskedTextBox1
-			// 
-			this.maskedTextBox1.Location = new System.Drawing.Point(67, 187);
-			this.maskedTextBox1.Mask = "90:00";
-			this.maskedTextBox1.Name = "maskedTextBox1";
-			this.maskedTextBox1.Size = new System.Drawing.Size(121, 20);
-			this.maskedTextBox1.TabIndex = 37;
-			this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+			this.button1.Size = new System.Drawing.Size(50, 46);
+			this.button1.TabIndex = 55;
+			this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// dateTimePicker1
 			// 
 			this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-			this.dateTimePicker1.Location = new System.Drawing.Point(67, 156);
+			this.dateTimePicker1.Location = new System.Drawing.Point(99, 147);
 			this.dateTimePicker1.Name = "dateTimePicker1";
-			this.dateTimePicker1.Size = new System.Drawing.Size(123, 20);
-			this.dateTimePicker1.TabIndex = 36;
+			this.dateTimePicker1.Size = new System.Drawing.Size(121, 20);
+			this.dateTimePicker1.TabIndex = 54;
 			// 
 			// comboBox2
 			// 
+			this.comboBox2.DisplayMember = "DoctorName";
 			this.comboBox2.FormattingEnabled = true;
-			this.comboBox2.Location = new System.Drawing.Point(67, 121);
+			this.comboBox2.Location = new System.Drawing.Point(99, 112);
 			this.comboBox2.Name = "comboBox2";
 			this.comboBox2.Size = new System.Drawing.Size(121, 21);
-			this.comboBox2.TabIndex = 35;
+			this.comboBox2.TabIndex = 53;
+			this.comboBox2.ValueMember = "DoctorID";
 			// 
 			// comboBox1
 			// 
+			this.comboBox1.DisplayMember = "DepartmentName";
 			this.comboBox1.FormattingEnabled = true;
-			this.comboBox1.Location = new System.Drawing.Point(67, 94);
+			this.comboBox1.Location = new System.Drawing.Point(99, 83);
 			this.comboBox1.Name = "comboBox1";
 			this.comboBox1.Size = new System.Drawing.Size(121, 21);
-			this.comboBox1.TabIndex = 34;
+			this.comboBox1.TabIndex = 52;
+			this.comboBox1.ValueMember = "DepartmentName";
 			// 
 			// textBox1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(67, 66);
+			this.textBox1.Location = new System.Drawing.Point(99, 30);
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(121, 20);
-			this.textBox1.TabIndex = 33;
+			this.textBox1.Size = new System.Drawing.Size(86, 20);
+			this.textBox1.TabIndex = 51;
 			// 
 			// label5
 			// 
 			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(-19, 190);
+			this.label5.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(2, 178);
 			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(78, 13);
-			this.label5.TabIndex = 32;
+			this.label5.Size = new System.Drawing.Size(90, 17);
+			this.label5.TabIndex = 50;
 			this.label5.Text = "Randevu Saati";
 			// 
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(-19, 163);
+			this.label4.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(2, 147);
 			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(80, 13);
-			this.label4.TabIndex = 31;
+			this.label4.Size = new System.Drawing.Size(94, 17);
+			this.label4.TabIndex = 49;
 			this.label4.Text = "Randevu Tarihi";
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(22, 129);
+			this.label3.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(43, 120);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(39, 13);
-			this.label3.TabIndex = 30;
+			this.label3.Size = new System.Drawing.Size(48, 17);
+			this.label3.TabIndex = 48;
 			this.label3.Text = "Doktor";
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(13, 102);
+			this.label2.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(34, 91);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(48, 13);
-			this.label2.TabIndex = 29;
+			this.label2.Size = new System.Drawing.Size(57, 17);
+			this.label2.TabIndex = 47;
 			this.label2.Text = "Poliklinik";
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(25, 73);
+			this.label1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(61, 33);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(21, 13);
-			this.label1.TabIndex = 28;
+			this.label1.Size = new System.Drawing.Size(23, 17);
+			this.label1.TabIndex = 46;
 			this.label1.Text = "TC";
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.SteelBlue;
+			this.button3.Location = new System.Drawing.Point(244, 12);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(518, 34);
+			this.button3.TabIndex = 48;
+			this.button3.Text = "GETİR";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.button3_Click_1);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(244, 91);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(518, 220);
+			this.dataGridView1.TabIndex = 47;
+			this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+			// 
+			// hOSPITALDataSet
+			// 
+			this.hOSPITALDataSet.DataSetName = "HOSPITALDataSet";
+			this.hOSPITALDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+			// 
+			// patientsBindingSource
+			// 
+			this.patientsBindingSource.DataMember = "Patients";
+			this.patientsBindingSource.DataSource = this.hOSPITALDataSet;
+			// 
+			// patientsTableAdapter
+			// 
+			this.patientsTableAdapter.ClearBeforeFill = true;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(257, 59);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(35, 13);
+			this.label7.TabIndex = 50;
+			this.label7.Text = "label7";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(99, 56);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(121, 20);
+			this.textBox3.TabIndex = 59;
 			// 
 			// Randevu
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(753, 450);
+			this.ClientSize = new System.Drawing.Size(824, 329);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.groupBox1);
 			this.Controls.Add(this.button3);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
 			this.Controls.Add(this.dataGridView1);
-			this.Controls.Add(this.maskedTextBox1);
-			this.Controls.Add(this.dateTimePicker1);
-			this.Controls.Add(this.comboBox2);
-			this.Controls.Add(this.comboBox1);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.label5);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label1);
 			this.Name = "Randevu";
 			this.Text = "Randevu";
 			this.Load += new System.EventHandler(this.Randevu_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.hOSPITALDataSet)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.patientsBindingSource)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -197,11 +292,12 @@ namespace HospitalAutomation
 
 		#endregion
 
-		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button button4;
 		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox2;
 		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.DataGridView dataGridView1;
-		private System.Windows.Forms.MaskedTextBox maskedTextBox1;
 		private System.Windows.Forms.DateTimePicker dateTimePicker1;
 		private System.Windows.Forms.ComboBox comboBox2;
 		private System.Windows.Forms.ComboBox comboBox1;
@@ -211,5 +307,12 @@ namespace HospitalAutomation
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private HOSPITALDataSet hOSPITALDataSet;
+		private System.Windows.Forms.BindingSource patientsBindingSource;
+		private HOSPITALDataSetTableAdapters.PatientsTableAdapter patientsTableAdapter;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox3;
 	}
 }
--- HospitalAutomation/Randevu.cs
+++ HospitalAutomation/Randevu.cs
@@ -21,6 +21,8 @@ namespace HospitalAutomation
 		BindingSource bs;
 		private void Randevu_Load(object sender, EventArgs e)
 		{
+			// TODO: This line of code loads data into the 'hOSPITALDataSet.Patients' table. You can move, or remove it, as needed.
+			this.patientsTableAdapter.Fill(this.hOSPITALDataSet.Patients);
 
 		}
 
@@ -34,6 +36,86 @@ namespace HospitalAutomation
 			bs.DataSource = dr;
 			dataGridView1.DataSource = bs;
 			con.Close();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (textBox1 != null)
+			{
+				string sorgu2 = "INSERT INTO Appointments(Patient_ID,Doctor_ID,AppointmentDate,AppointmentTime,Department) VALUES (@d_id,@p_id,@date,@time,@dep)";
+				SqlCommand cmd = new SqlCommand(sorgu2, con);
+				cmd.Parameters.AddWithValue("@p_id", textBox3.Text);
+				cmd.Parameters.AddWithValue("@d_id", comboBox2.SelectedValue);
+				cmd.Parameters.AddWithValue("@date", dateTimePicker1.Value);
+				cmd.Parameters.AddWithValue("@time", textBox2.Text);
+				cmd.Parameters.AddWithValue("@dep", comboBox1.SelectedValue);
+
+				con.Open();
+				cmd.ExecuteNonQuery();
+				MessageBox.Show("İşlem Başarılı");
+				con.Close();
+			}
+			else
+			{
+				MessageBox.Show("Lütfen bilgileri doldurunuz.");
+			}
+		}
+
+		private void button3_Click_1(object sender, EventArgs e)
+		{
+			string sorgu = "Select AppointmentID,p.PatientTC,Department,d.DoctorName+' '+d.DoctorLastname as [DoctorFullName],p.PatientName+' '+p.PatientLastname as [PatientFullName],AppointmentDate,AppointmentTime from Appointments a inner join Doctors d ON a.Doctor_ID = d.DoctorID inner join Patients p ON a.Patient_ID = p.PatientID";
+			SqlCommand cmd = new SqlCommand(sorgu, con);
+			con.Open();
+			bs = new BindingSource();
+			SqlDataReader dr = cmd.ExecuteReader();
+
+			bs.DataSource = dr;
+			dataGridView1.DataSource = bs;
+			con.Close();
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			if (textBox1.Text != null)
+			{
+				string sorgu1 = "select * from Patients where PatientTC = @patTC";
+			
+				SqlDataAdapter dap = new SqlDataAdapter(sorgu1, con);
+				dap.SelectCommand.Parameters.AddWithValue("@patTC", textBox1.Text);
+				DataSet ds = new DataSet();
+				dap.Fill(ds);
+
+			    if (ds.Tables[0].Rows.Count > 0)
+				{
+					textBox1.Text = ds.Tables[0].Rows[0][1].ToString();
+					label7.Text = textBox1.Text;
+			
+				}
+
+
+
+				//string sorgu3 = "select * from Patients where PatientTC = @patTC";
+				//SqlCommand command = new SqlCommand(sorgu3, con);
+				//command.Parameters.AddWithValue("@patTC", textBox1.Text);
+				//con.Open();
+				//SqlDataReader dr = command.ExecuteReader();
+				//DataTable dt = new DataTable();
+				//dt.Load(dr);
+				//dataGridView1.DataSource = dt;
+				//con.Close();
+			}
+			else
+			{
+				MessageBox.Show("hasta bulunamadı");
+			}
+		}
+
+		private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+		{
+			string id =(dataGridView1.SelectedRows[0].Cells[1].Value).ToString();
+			
+			comboBox3.SelectedItem= dataGridView1.SelectedRows[0].Cells[4].Value;
+
 		}
 	}
 }
